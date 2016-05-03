6453a9689a98e5eeb458860e22c4c2dfb687bdb4
Tim Berners-Lee <timbl+git@w3.org>
2016-05-03 14:20:41 -0400

    Now structured using node modules and more or less working on tab ff ext

---
+++ js/init/init-node.js
@@ -0,0 +1,13 @@
+// init-node.js
+// This is a data mashup library
+
+var log = require('../tab/log-ext-node.js')
+
+var UI = require('../solid-ui/index.js')
+
+var panes = require('../panes/index.js')
+
+UI.log = log
+UI.panes = panes
+
+// init-node ends
